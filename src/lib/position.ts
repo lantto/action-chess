7a2e00efa43e2dbbc8a1e0576af97812cfe93625
Erik Lantto <lantto@gmail.com>
2018-03-27 10:26:32 +0200

    @damoonrashidi

--- src/lib/position.ts
+++ src/lib/position.ts
@@ -9,7 +9,7 @@ export class GridPosition {
 
   set position (value: Vector2D) {
     if (value.x < 0 || value.x > 7 || value.y < 0 || value.y > 7) {
-      throw new Error(`GridPosition value was set out of bounds. was ${value} but must be in 0..7`);
+      throw new Error(`GridPosition value was set out of bounds. Was ${value} but must be in 0..7`);
     }
   }
 
